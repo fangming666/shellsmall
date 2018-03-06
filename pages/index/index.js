a6a690a4622467db41368084dca10edcd35efb72
房明 <1217215022@qq.com>
2018-03-06 18:10:06 +0800

    success

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,67 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+const app = getApp();
+// 动画
 Page({
   data: {
-    allRate: 0
+    allRate: 0,
+    allGoodsNum: 0,
+    headNotice: "优惠信息",
+    headTitle: "商品",
+    goods: [{
+      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
+    }, {
+      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
+    }, {
+      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
+    }, {
+      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
+    }, {
+      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
+    }, {
+      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
+    }, {
+      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
+    }, {
+      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
+    }],
+
   },
 
   onLoad: function () {
 
   },
+  reduceNum(e) {
+    let index = e.currentTarget.dataset.item;
+    let allNum = this.data.allGoodsNum;
+    let resultArr = this.data.goods;
+    if (resultArr[index].num == 0) {
+      resultArr[index].num = 0;
+
+    } else {
+      resultArr[index].num = resultArr[index].num - 1;
+      this.setData({
+        allGoodsNum: allNum - 1
+      })
+    };
+    this.setData({
+      goods: resultArr
+    })
+  },
+  addNum(e) {
+    let index = e.currentTarget.dataset.item;
+    let resultArr = this.data.goods;
+    let allNum = this.data.allGoodsNum;
+    if (resultArr[index].num == resultArr[index].allNum) {
+      resultArr[index].num = resultArr[index].allNum
+    } else {
+      resultArr[index].num = resultArr[index].num + 1;
+      this.setData({
+        allGoodsNum: allNum + 1
+      })
+    };
+    this.setData({
+      goods: resultArr
+    })
+  }
 })
