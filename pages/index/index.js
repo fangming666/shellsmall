dff47f975e9ad394d29d893acebbb589ebcc425f
房明 <1217215022@qq.com>
2018-03-07 17:51:24 +0800

    success

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,8 +8,16 @@ Page({
     allGoodsNum: 0,
     headNotice: "优惠信息",
     headTitle: "商品",
+    cardSwitch: false,
+    afficheSwicth: false,
+    afficheTitle: "这是一个商品的名称",
+    afficheInit: 2000,
+    afficheNow: 1000,
+    afficheSold: 10,
+    affichePrefer: "这是一个优惠的信息",
+    afficheInfo: "这是一个公告的信息",
     goods: [{
-      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
+      "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": "", "affichePrefer": "这是一个真的优惠信息", "afficheInfo": "这是一个真的公告信息"
     }, {
       "img": "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/151fd1ab20c7d69f611a07096a65b672_121_121.jpg", "name": "这是一个商品的名称", "sold": "10", "init": "2000", "now": "1000", "allNum": 11, "num": 0, "animationData": ""
     }, {
@@ -62,6 +70,42 @@ Page({
     };
     this.setData({
       goods: resultArr
+    })
+  },
+
+  toggleCard() {
+    let cardSwitchS = this.data.cardSwitch
+    this.setData({
+      cardSwitch: !cardSwitchS
+    });
+
+  },
+  clooseCard() {
+    this.setData({
+      cardSwitch: false
+    })
+  },
+  clooseAffiche() {
+    console.log(this.data.afficheSwicth);
+    this.setData({
+      afficheSwicth: false
+    })
+  },
+  openAfficheOne() {
+    this.setData({
+      afficheSwicth: true
+    })
+  },
+  openAfficheTwo(e) {
+    let index = e.currentTarget.dataset.item;
+    this.setData({
+      afficheTitle: this.data.goods[index].name,
+      afficheInit: this.data.goods[index].init,
+      afficheNow: this.data.goods[index].now,
+      afficheSold: this.data.goods[index].sold,
+      affichePrefer: this.data.goods[index].affichePrefer,
+      afficheInfo: this.data.goods[index].afficheInfo,
+      afficheSwicth: true
     })
   }
 })
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,67 @@
 // pages/my/my.js
+const app = getApp(); //设置全局变量
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    userInfo: {},
+    hasUserInfo: false,
+    myList: [{ "href": "/pages/order/order", "text": "name1", "img": "http://fm.xiaofany.com/shellSmall/dingDan.jpg" },
+    { "href": "/pages/key/key", "text": "name2", "img": "http://fm.xiaofany.com/shellSmall/key.jpg" },
+    { "href": "/pages/news/news", "text": "name3", "img": "http://fm.xiaofany.com/shellSmall/xiaoXi.jpg" }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        if (!res.userInfo.avatarUrl) {
+          res.userInfo.avatarUrl = "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3289761550,697278018&fm=27&gp=0.jpg"
+        }
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          if (!res.userInfo.avatarUrl) {
+            res.userInfo.avatarUrl = "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3289761550,697278018&fm=27&gp=0.jpg"
+          }
+          app.globalData.userInfo = res.userInfo;
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  getMyInfo() {
+    this.setData({
+      mySculpture: globalData.userInfo.avatarUrl,
+      myName: globalData.userInfo.nickName
+    })
   }
 })
